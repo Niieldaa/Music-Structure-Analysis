e8328e01e740d6d6b6d95af4ed64b6def9ff1eb4
AwesomeAnna19 <annaroseyt13@gmail.com>
2025-03-17 10:04:15 +0100

    Checking if the code takes all 4 columns from the CSV files

--- NoveltyBasedSegmentation.py
+++ NoveltyBasedSegmentation.py
@@ -16,13 +16,23 @@ import libfmp.c4
 
 # Annotation
 fn_ann = os.path.join('Files', 'QM Parsed data', 'Left', 'parsed_VitalicQM1L.csv')
-ann, color_ann = libfmp.c4.read_structure_annotation(fn_ann, fn_ann_color=fn_ann)
+df = pd.read_csv(fn_ann, sep=',', keep_default_na=False, header=0)
+print('CSV columns detected:', df.columns)
+expected_columns = ['TIME', 'VALUE', 'DURATION', 'LABEL']
+if not all(col in df.columns for col in expected_columns):
+    raise ValueError(f'ERROR: Expected columns {expected_columns}, but found {df.columns}.')
+start_times = df['TIME'].astype(float)
+end_times = df['TIME'].astype(float) + df['DURATION'].astype(float)
+labels = df['LABEL']
+ann = list(zip(start_times, end_times, labels))
+color_ann = None
+#ann, color_ann = libfmp.c4.read_structure_annotation(fn_ann, fn_ann_color=fn_ann)
 
 # SM
 fn_wav = os.path.join('DO NOT TOUCH', 'Audio Files', 'Emmanuel', '01. Vitalic - Polkamatic.wav')
 tempo_rel_set = libfmp.c4.compute_tempo_rel_set(0.66, 1.5, 5)
 x, x_duration, X, Fs_X, S, I = libfmp.c4.compute_sm_from_filename(fn_wav,
-                                                L=81, H=10, L_smooth=1, thresh=1)
+                                                                  L=81, H=10, L_smooth=1, thresh=1, Fs=None)
 
 # Visualization
 ann_frames = libfmp.c4.convert_structure_annotation(ann, Fs=Fs_X)
