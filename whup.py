875f9cf021da82f8f7c2a7d5f8fef7f612d1a9c0
Malthezz <malthh21@student.aau.dk>
2025-05-05 13:57:21 +0200

    Chromagram med frid

--- whup.py
+++ whup.py
@@ -1,10 +1,13 @@
 import librosa
 import librosa.display
+import librosa.feature
 import matplotlib.pyplot as plt
 import numpy as np
 
+import ruptures as rpt
+
 # Load a sample audio file (Nutcracker, short clip)
-y, sr = librosa.load(librosa.example('nutcracker'), duration=10)
+y, sr = librosa.load(r"DO NOT TOUCH/Audio Files/Emmanuel/01. Vitalic.flac", sr = 4000)
 
 # Compute the chromagram using STFT
 chroma = librosa.feature.chroma_stft(y=y, sr=sr)
@@ -15,14 +18,6 @@ chroma_norm = librosa.util.normalize(chroma, axis=0)
 # Compute self-similarity matrix (cosine similarity)
 ssm = np.dot(chroma_norm.T, chroma_norm)
 
-# Plot the chromagram
-plt.figure(figsize=(10, 4))
-librosa.display.specshow(chroma, x_axis='time', y_axis='chroma', cmap='coolwarm')
-plt.colorbar(label='Intensity')
-plt.title('Chromagram (STFT-based)')
-plt.tight_layout()
-plt.show()
-
 # Plot the self-similarity matrix (SSM)
 plt.figure(figsize=(8, 8))
 plt.imshow(ssm, origin='lower', cmap='magma', interpolation='nearest')
@@ -32,4 +27,3 @@ plt.ylabel('Time Frame')
 plt.colorbar(label='Cosine Similarity')
 plt.tight_layout()
 plt.show()
-
